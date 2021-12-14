307547520651f23bba8da1567d1e8a34991ae2d2
vanemcb <vanem2201@gmail.com>
2021-12-14 14:07:30 -0500

    New project created

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -2,11 +2,14 @@
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+if __name__ == "__main__":
+    boxes = []
 
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+    keys = []
+    for n in range(1, 1000):
+        keys = []
+        for m in range(1, 1000):
+            keys.append(m)
+        boxes.append(keys)
 
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+    print(canUnlockAll(boxes))
