af16e1b91dc53a187a73e9deac8b40ef4ae48a41
vanemcb <vanem2201@gmail.com>
2022-01-06 20:16:45 -0500

    Task 0 created

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Method minOperations
+"""
+
+def minOperations(n):
+
+    if type(n) is not int or n <= 0:
+        return 0
+
+    oper = 0
+    char = 1
+    copy = 1
+
+    while char < n:
+        if n % char == 0:
+            copy = char
+            oper += 1
+        if char != n:
+            char += copy
+            oper += 1
+        else:
+            break
+
+    return oper
