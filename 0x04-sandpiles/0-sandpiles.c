5eb35ae6ae600b128c9910a234cdae8789b9853f
vanemcb <vanem2201@gmail.com>
2022-01-19 20:57:14 -0500

    task 0 finished

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,11 +8,94 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	for (i = 0; i <= 3; i++)
+	add_sandpile(grid1, grid2);
+	/* Topple piles accordingly when grid is unstable and print each time */
+	while (!is_stable(grid1))
 	{
-		for (x = 0; x <= 3; x++)
+		// print_grid(grid1);
+		topple(grid1, grid2);
+	}
+}
+
+/**
+ * is_stable - Check if square has more than 3 grains
+ * @grid: 3 x 3 grid
+ * Return: 0 if stable, 1 if unstable
+ */
+int is_stable(int grid[3][3])
+{
+	int row, col;
+	for (row = 0; row < 3; row++)
+	{
+		for (col = 0; col < 3; col++)
 		{
-			grid1[i][x] = 
+			if (grid[row][col] > 3)
+			{
+				return (0);
+			}
+		}
+	}
+	return (1);
+}
+
+/**
+ * topple - Partitions grains from squares greater than 3 *
+ * @grid1: 3 x 3 board *
+ * @grid2: 3 x 3 board *
+ * Return: Void
+ */
+void topple(int grid1[3][3], int grid2[3][3])
+{
+	int row, col;
+	for (row = 0; row < 3; row++)
+	{
+		for (col = 0; col < 3; col++)
+		{
+			if (grid1[row][col] > 3)
+			{
+				/* Send one grain of sand off the grid */
+				grid2[row][col] -= 4;
+				/* Send one grain of sand up */
+				if (row - 1 >= 0)
+				{
+					grid2[row - 1][col]++;
+				}
+				/* Send one grain of sand down */
+				if (row + 1 <= 2)
+				{
+					grid2[row + 1][col]++;
+				}
+				/* Send one grain of sand left */
+				if (col - 1 >= 0)
+				{
+					grid2[row][col - 1]++;
+				}
+				/* Send one grain of sand right */
+				if (col + 1 <= 2)
+				{
+					grid2[row][col + 1]++;
+				}
+			}
+		}
+	}
+	add_sandpile(grid1, grid2);
+}
+
+/**
+ * add_sandpile - Add two 3 x 3 grid sandpiles
+ * @grid1: 3 x 3 board
+ * @grid2: 3 x 3 board
+ * Return: Return value
+ */
+void add_sandpile(int grid1[3][3], int grid2[3][3])
+{
+	int row, col;
+	for (row = 0; row < 3; row++)
+	{
+		for (col = 0; col < 3; col++)
+		{
+			grid1[row][col] += grid2[row][col];
+			grid2[row][col] = 0;
 		}
 	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,5 +5,9 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int is_stable(int grid[3][3]);
+void topple(int grid1[3][3], int grid2[3][3]);
+void add_sandpile(int grid1[3][3], int grid2[3][3]);
+
 
 #endif /* SANDPILES_H */
