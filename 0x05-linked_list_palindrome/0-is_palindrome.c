660d7025f46f0a510de29a89e58f3e1068f64223
vanemcb <vanem2201@gmail.com>
2022-01-19 21:23:40 -0500

    Betty style fixed

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -23,7 +23,7 @@ int is_palindrome(listint_t **head)
 int checkPalindrome(listint_t **left, listint_t *right)
 {
 	if (right == NULL)
-		return 1;
+		return (1);
 
 	int result = checkPalindrome(left, right->next) && ((*left)->n == right->n);
 	(*left) = (*left)->next;
