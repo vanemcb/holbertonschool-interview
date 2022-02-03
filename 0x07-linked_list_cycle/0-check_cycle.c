04979afa1135913590cb8c1f4bb8c33bfcc59523
vanemcb <vanem2201@gmail.com>
2022-02-03 14:34:54 -0500

    Task 0 created

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: the list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_p = list, *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
