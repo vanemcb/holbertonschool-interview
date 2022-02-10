54aa532f6ec4f74ffce1d581dbf7800a55a37cdb
vanemcb <vanem2201@gmail.com>
2022-02-10 15:29:19 -0500

    Task 0 fixed

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,9 +1,27 @@
-#ifndef PALINDROME_H
-#define PALINDROME_H
+#include "palindrome.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ * is_palindrome - function that checks whether or not a given
+ * unsigned integer is a palindrome
+ * @n: is the number to be checked
+ * Return: 1 if n is a palindrome, and 0 otherwise
+ */
 
-int is_palindrome(unsigned long n);
+int is_palindrome(unsigned long n)
+{
+	int num_rev = 0, mod, num;
 
-#endif /* PALINDROME_H */
+	num = n;
+
+	while (n != 0)
+	{
+		mod = n % 10;
+		num_rev = num_rev * 10 + mod;
+		n /= 10;
+	}
+
+	if (num == num_rev)
+		return (1);
+
+	return (0);
+}
