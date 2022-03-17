e638107baf9c0c818eab92367136126fbe2bbdb3
vanemcb <vanem2201@gmail.com>
2022-03-17 18:08:36 -0500

    task 0 created

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys
+
+
+def start(en):
+    solutions = []
+    for row in range(0, en):
+        solve(en, 0, row, solutions)
+    for solution in solutions:
+        print(solution)
+
+
+def solve(size, col, row, sol_set, sol=[]):
+    e = evaluate(col, row, sol)
+    if e is False:
+        return
+    s = sol.copy()
+    s.append([col, row])
+    if col == size - 1:
+        accept(sol_set, s)
+    for new_row in range(0, size):
+        solve(size, col + 1, new_row, sol_set, s)
+
+
+def evaluate(col, row, sol):
+    for queen in sol:
+        if queen[0] == col or queen[1] == row:
+            return False
+        if queen[0] - queen[1] == col - row:
+            return False
+        if queen[0] + queen[1] == col + row:
+            return False
+    return True
+
+
+def accept(sol_set, sol):
+    sol_set.append(sol)
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N\n")
+    exit(1)
+else:
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4\n")
+            exit(1)
+        start(n)
+    except Exception:
+        print("N must be a number\n")
+        exit(1)
